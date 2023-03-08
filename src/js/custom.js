07aa27ab8ef72ce78685a5aaa6504dbe46af973c
JamesSwarge <james.swarge@gmail.com>
2023-03-08 17:40:10 +0530

    Homepage and About page changes.

--- src/js/custom.js
+++ src/js/custom.js
@@ -25,15 +25,16 @@ $(document).ready(function () {
   //owl carousel
   $('.product-cards-carousel').owlCarousel({
     autoplay: true,
-    autoplayTimeout: 1500,
-    autoplayHoverPause: true,
-    mouseDrag: false,
-    loop: false,
+    autoplayTimeout: 360000,
+    autoplayHoverPause: false,
+    mouseDrag: true,
+    loop: true,
     margin: 10, 
-    nav: false,
-    dots: false,
+    nav: true,
+    dots: true,
     margin: 16,
     items: 3.5,
+    navText: ['<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="19.5" transform="matrix(-1 0 0 1 40 0)" stroke="white"/><g clip-path="url(#clip0_101_244)"><path d="M29.3711 19.9297H11.3152" stroke="white" stroke-width="1.5" stroke-linecap="square"/><path d="M15.9336 14.8906L10.8947 19.9295" stroke="white" stroke-width="1.5" stroke-linecap="square"/><path d="M15.9336 24.9685L10.8947 19.9297" stroke="white" stroke-width="1.5" stroke-linecap="square"/></g><defs><clipPath id="clip0_101_244"><rect width="19.9963" height="11.8594" fill="white" transform="matrix(-1 0 0 1 30 14)"/></clipPath></defs></svg>', '<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="19.5" stroke="white"/><g clip-path="url(#clip0_101_252)"><path d="M10.6289 19.9297H28.6848" stroke="white" stroke-width="1.5" stroke-linecap="square"/><path d="M24.0664 14.8906L29.1053 19.9295" stroke="white" stroke-width="1.5" stroke-linecap="square"/><path d="M23.9961 25.0389L29.035 20" stroke="white" stroke-width="1.5" stroke-linecap="square"/></g><defs><clipPath id="clip0_101_252"><rect width="19.9963" height="11.8594" fill="white" transform="translate(10 14)"/></clipPath></defs></svg>'],
     responsive: {
       0: {
         items: 1.3,
@@ -57,10 +58,66 @@ $(document).ready(function () {
 
   //header mobile
 
-  if($(window).width() < 767) {
-    
-  } else {
-    
-  }
+  // $('html, body').animate({
+  //   scrollTop: ($('.pattern').first().offset().top)
+  // }, 500);
+
+  // const navHeight = $('.navbar').height();
+  // console.info("asd1", navHeight)
+  // var mainbottom = $('.header-nav').offset().top + navHeight + 200;
+  // console.info("asd2", mainbottom)
+  // $(window).on('scroll', function () {
+  //   // console.info("scrolled")
+  //   stop = Math.round($(window).scrollTop());
+  //   if (stop > mainbottom) {
+  //     $('#header-nav').addClass('navbar-fixed-top');
+  //     console.info("up")
+  //   } else {
+  //     $('#header-nav').removeClass('navbar-fixed-top');
+  //     console.info("up2")
+  //   }
+  // });
+
+  const body = document.body;
+  let lastScroll = 200;
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+      body.classList.remove("scroll-up");
+      return;
+    }
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+      body.classList.remove("scroll-up");
+      setTimeout(function(){
+        body.classList.add("scroll-down")
+      }, 600);
+    } else if (
+      currentScroll < lastScroll &&
+      body.classList.contains("scroll-down")
+    ) {
+      body.classList.remove("scroll-down");
+      body.classList.add("scroll-up");
+    }
+    lastScroll = currentScroll;
+  });
 
 });
+
+// const header = document.querySelector(".navbar")
+// const firstSection = document.querySelector(".first-sec")
+
+// const sectionOneOptions = {
+//   rootMargin: "-200px 0px 0px 0px"
+// };
+
+// const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
+//   entries.forEach(entry => {
+//     if (!entry.isIntersecting) {
+//       header.classList.add("nav-scrolled");
+//     } else {
+//       header.classList.remove("nav-scrolled");
+//     }
+//   });
+// }, sectionOneOptions);
+
+// sectionOneObserver.observe(firstSection);
